0ca73e7477ffe98f0ead858784a27cca5b2b01dc
hamstap85 <giff.h92@gmail.com>
2015-12-07 00:27:12 -0500

    didn't see part 2 originally

--- 01/01.py
+++ 01/01.py
@@ -12,3 +12,19 @@ input_file.close()
 
 print(data.count('(') - data.count(')'))
 
+
+# In[21]:
+
+i = 0
+floor = 0
+while True:
+    c = data[i]
+    if c == "(":
+        floor += 1
+    elif c == ")":
+        floor -= 1
+    i += 1
+    if floor == -1:
+        break
+print(i)
+
